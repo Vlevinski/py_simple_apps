544efd24e23a91a977e4405b07a045508924b68a
Vlevinski <v.levinski@gmail.com>
2024-06-17 08:54:53 +0300

    first commit

--- argparse_script/script.py
+++ argparse_script/script.py
@@ -1,12 +1,18 @@
 import argparse
 
-# Create the parser
-parser = argparse.ArgumentParser(description='Process some strings.')
+def process_message(message=None):
+    if message is None:
+        # Create the parser
+        parser = argparse.ArgumentParser(description='Process some strings.')      
+        parser.add_argument('message', type=str, help='The message to display') 
+        args = parser.parse_args() # Execute the parse_args() method
+        message = args.message
 
-# Add the arguments
-parser.add_argument('message', type=str, help='The message to display')
+    # Print the message
+    print(message)
 
-# Execute the parse_args() method
-args = parser.parse_args()
+# Example usage from a script or another function
+if __name__ == '__main__':
+    # This will print the provided message
+    process_message("Hello, World!")  
 
-print(args.message)
