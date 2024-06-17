3235ef5b5bdfb9789eeb1c27e0b4a666df91cdfb
Vlevinski <v.levinski@gmail.com>
2024-06-17 08:58:10 +0300

    first commit

--- argparse_script/script.py
+++ argparse_script/script.py
@@ -3,16 +3,21 @@ import argparse
 def process_message(message=None):
     if message is None:
         # Create the parser
-        parser = argparse.ArgumentParser(description='Process some strings.')      
-        parser.add_argument('message', type=str, help='The message to display') 
-        args = parser.parse_args() # Execute the parse_args() method
+        parser = argparse.ArgumentParser(description='Process some strings.')
+        # Add the argument
+        parser.add_argument('message', type=str, help='The message to display')
+        # Execute the parse_args() method
+        args = parser.parse_args()
         message = args.message
 
     # Print the message
     print(message)
 
-# Example usage from a script or another function
 if __name__ == '__main__':
-    # This will print the provided message
-    process_message("Hello, World!")  
+    # Example usage: This will use argparse to get the message from the command line
+    # If you want to test the function with a predefined message, uncomment the following line
+    # process_message("Hello, World!")
+    
+    # This will use argparse to get the message from the command line
+    process_message() 
 
